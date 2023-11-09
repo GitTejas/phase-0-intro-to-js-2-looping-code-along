71297d1916e33a6a9ea71fa068fc792d8e91c354
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-08 22:02:55 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function writeCards(names, message="cool") {
+    const respectiveArray = []
+    for (let i= 0; i<names.length; i++) {
+    respectiveArray.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+    }
+    return respectiveArray
+}
+
+
 // Code your solutions in this file
+
+
+
+function countDown(number) {
+    while (number>=0) {
+        console.log(number--)
+    }
+}
